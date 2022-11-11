9e263354413b86cc785931d9a59dcfe034527cbf
Sloanstar <jason.a.sloan@gmail.com>
2022-11-11 03:00:27 -0500

    Basic function to parse zones/*.zf and process them as new zones.

--- parse-zonefile.py
+++ parse-zonefile.py
@@ -3,32 +3,53 @@ import requests
 import dns.rdataset
 import dns.zonefile
 import dns.zone
+import os, glob
+import json
+import re
+from tools.helpers import *
+from tools.f5 import *
 
-def readSecret(secretPath):
-    with open(secretPath) as f:
-        mySecret = f.readline().strip()
-        return mySecret
 
-api_url = "https://{}.console.ves.volterra.io/api/config/dns/namespaces/system/dns_zones".format(readSecret(".secrets/.consoleDomain"))
+def main():
+    api_url = "https://{}.console.ves.volterra.io/api/config/dns/namespaces/system/dns_zones".format(readSecret(".secrets/.consoleDomain"))
 
-api_headers = {
-    "Authorization" :   "{}".format(readSecret(".secrets/.apiToken")),
-    "Accept"        :   "application/json"
-}
+    api_headers = {
+        "Authorization" :   "{}".format(readSecret(".secrets/.apiToken")),
+        "Accept"        :   "application/json"
+    }
 
-zonesResp = requests.get(api_url, headers=api_headers, verify=False)
-#print (response.json())
+    path = 'zones/'
+    for filename in glob.glob(os.path.join(path, '*.zf')):
+        with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
+            dnsZone = dns.zone.from_file(f)
+            defaultRR = ZoneNodesToF5Json(dnsZone)
+            zoneName = re.sub('\.zf', '', os.path.basename(filename))
+            print("Processing Zone {}".format(zoneName))
 
-zonesJson = zonesResp.json()
-for zonesItem in zonesJson['items']:
-    #print("Zone Ftch: " + api_url + "/" + zonesItem['name'])
-    zoneResp = requests.get(api_url + "/" + zonesItem['name'], headers=api_headers, verify=False)
-    zoneJson = zoneResp.json()
-    #print(zoneJson['metadata'])
-    #print(zoneJson['spec']['secondary']['zone_file'])
-    f = open("", "rt")
-    #dnsZone = dns.zonefile.read_rrsets(zoneJson['spec']['secondary']['zone_file'], origin=zonesItem['name'], relativize=True, rdclass=None)
-    dnsZone = dns.zone.from_file(f)
-    print(dnsZone.to_text())
-    #for zoneItem in zoneJson['items']:
-        #print(zoneItem)
+            jBody = {
+                "metadata": {
+                    "name": zoneName,
+                    "namespace": "system",
+                    "labels": {},
+                    "annotations": {},
+                    "disable": False
+                },
+                "spec": {
+                    "primary": {
+                        "default_soa_parameters": {},
+                        "dnssec_mode": {
+                            "disable": {}
+                        },
+                        "rr_set_group": [],
+                        "default_rr_set_group": defaultRR
+                    }
+                }
+            }
+        #print(json.dumps(jBody, indent=4))
+        print("Attempting to create zone {} at:\n{}".format(zoneName, api_url))
+        createZone = requests.post(api_url, verify=False, headers=api_headers, json=jBody)
+
+        print(createZone.json())
+
+if __name__ == "__main__":
+    main()
